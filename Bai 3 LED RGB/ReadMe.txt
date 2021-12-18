f46db3512f2e67744e20d8aa3e059ede92b964af
ToanPhong <phong.ctt.60cntt@ntu.edu.vn>
2021-12-18 21:54:51 +0700

    First commit

---
+++ Bai 3 LED RGB/ReadMe.txt
@@ -0,0 +1,2 @@
+o	Bài này thực hiện việc lập trình bật 1 đèn LED RGB với 
+3 điện trở R1, R2 R3, đèn LED được kết nối thông qua điện trở lần lượt vào chân số 9, 10,11 của board mạch.
---
+++ Bai 3 LED RGB/code/sketch_oct02a/sketch_oct02a.ino
@@ -0,0 +1,30 @@
+int counter;
+
+void setup()
+{
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  analogWrite(11, 255);
+  analogWrite(10, 0);
+  analogWrite(9, 0);
+  delay(1000); // Wait for 1000 millisecond(s)
+  analogWrite(11, 255);
+  analogWrite(10, 255);
+  analogWrite(9, 255);
+  delay(1000); // Wait for 1000 millisecond(s)
+  for (counter = 0; counter < 10; ++counter) {
+    analogWrite(11, 255);
+    analogWrite(10, 255);
+    analogWrite(9, 0);
+    delay(1000); // Wait for 1000 millisecond(s)
+    analogWrite(11, 51);
+    analogWrite(10, 255);
+    analogWrite(9, 51);
+    delay(1000); // Wait for 1000 millisecond(s)
+  }
+}
